b97742a832f3e09a28a04193b3be15628ef9aae9
nguye <20521678@gmail.com>
2023-11-08 11:12:50 +0700

    two commit

--- app/src/main/java/com/example/onbroading_login/LoginActivity.java
+++ app/src/main/java/com/example/onbroading_login/LoginActivity.java
@@ -2,33 +2,83 @@ package com.example.onbroading_login;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.InputType; // Để sử dụng InputType
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
 public class LoginActivity extends AppCompatActivity {
 
     private EditText editTextLogInEmail, editTextLogInPassWord;
     private Button buttonLogInLogIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ImageView backHomeFromLogin = findViewById(R.id.img_loginBack);
+        backHomeFromLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        TextView SignUptoLogin = findViewById(R.id.txt_toSignUp);
+        SignUptoLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+            startActivity(intent);
+        });
+
+        TextView LogintoForgotPassword = findViewById(R.id.txt_toForgotPassword);
+        LogintoForgotPassword.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, ForgotPassword.class);
+            startActivity(intent);
+        });
+
+        editTextLogInEmail = findViewById(R.id.edt_loginEmail);
+        editTextLogInPassWord = findViewById(R.id.edt_loginPassword);
+        buttonLogInLogIn = findViewById(R.id.btn_loginLogIn);
+
+
+        ImageView passwordVisibilityToggle = findViewById(R.id.img_passwordVisibility);
+        EditText passwordEditText = findViewById(R.id.edt_loginPassword);
+
+        final boolean[] passwordVisible = {false};
+
+        passwordVisibilityToggle.setOnClickListener(v -> {
+            passwordVisible[0] = !passwordVisible[0];
+
+            if (passwordVisible[0]) {
+                passwordEditText.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                passwordVisibilityToggle.setImageResource(R.drawable.visible); // Thay đổi biểu tượng hiện mật khẩu
+            } else {
+                passwordEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                passwordVisibilityToggle.setImageResource(R.drawable.invisible); // Thay đổi biểu tượng ẩn mật khẩu
+            }
+
+            // Đảm bảo rằng văn bản trong EditText không bị mất
+            passwordEditText.setSelection(passwordEditText.length());
+        });
+
+        buttonLogInLogIn.setOnClickListener(view -> {
+            if (!validateEmail() | !validatePassWord() ) {
+
+            }
+            else {
+                login();
+            }
+        });
+    }
 
     private Boolean validateEmail() {
         String val = editTextLogInEmail.getText().toString();
@@ -60,61 +110,24 @@ public class LoginActivity extends AppCompatActivity {
         API_Interface authService = retrofit.create(API_Interface.class);
         String username = editTextLogInEmail.getText().toString();
         String password = editTextLogInPassWord.getText().toString();
-        Call<HelperClass> call = authService.login("openremote",username,password,"password");
+        Call<HelperClass> call = authService.login("openremote", username, password, "password");
         call.enqueue(new Callback<HelperClass>() {
             @Override
             public void onResponse(Call<HelperClass> call, Response<HelperClass> response) {
                 if (response.isSuccessful()) {
                     HelperClass loginResponse = response.body();
                     String accessToken = loginResponse.getAccess_token();
-                    Log.d("token",accessToken);
                     Toast.makeText(LoginActivity.this, "Success", Toast.LENGTH_SHORT).show();
-                }
-                else {
+                } else {
                     // Authentication failed
                     Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
                 }
             }
+
             @Override
             public void onFailure(Call<HelperClass> call, Throwable t) {
                 // Handle network or API errors
-                Log.d("API CALL", t.getMessage().toString());
                 Toast.makeText(LoginActivity.this, "Login Failed: " + t.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        ImageView backHomeFromLogin = findViewById(R.id.img_loginBack);
-        backHomeFromLogin.setOnClickListener(v -> {
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            startActivity(intent);
-        });
-
-        TextView SignUptoLogin = findViewById(R.id.txt_toSignUp);
-        SignUptoLogin.setOnClickListener(v -> {
-            Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
-            startActivity(intent);
-        });
-
-        TextView LogintoForgotPassword = findViewById(R.id.txt_toForgotPassword);
-        LogintoForgotPassword.setOnClickListener(v -> {
-            Intent intent = new Intent(LoginActivity.this, ForgotPassword.class);
-            startActivity(intent);
-        });
-
-        editTextLogInEmail = findViewById(R.id.edt_loginEmail);
-        editTextLogInPassWord = findViewById(R.id.edt_loginPassword);
-        buttonLogInLogIn = findViewById(R.id.btn_loginLogIn);
-        buttonLogInLogIn.setOnClickListener(view -> {
-            if (!validateEmail() | !validatePassWord() ) {
-            }
-            else {
-                login();
             }
         });
     }
--- app/src/main/java/com/example/onbroading_login/SignUpActivity.java
+++ app/src/main/java/com/example/onbroading_login/SignUpActivity.java
@@ -1,10 +1,16 @@
 package com.example.onbroading_login;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -12,16 +18,26 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
+import java.net.URISyntaxException;
+import java.util.Locale;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    private EditText editTextSignUpUserName, editTextSignUpEmail, editTextSignUpPassWord, editTextSignUpConfirmPassword;
+    private EditText editTextSignUpUserName, editTextSignUpEmail, editTextSignUpPassWord, editTextSignUpConfirmPassword,editTextLogInEmail,editTextLogInPassWord;
     private Button buttonSignUpCreateAccount;
     private ProgressBar progressbar;
-    private FirebaseDatabase database;
-    private DatabaseReference reference;
+    private boolean shouldStopEvaluation=false;
+
+    private WebView webView;
 
     private Boolean validateUserName() {
         String val = editTextSignUpUserName.getText().toString();
@@ -93,6 +109,105 @@ public class SignUpActivity extends AppCompatActivity {
             return true;
         }
     }
+
+    private void login() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://uiot.ixxc.dev/auth/realms/master/protocol/openid-connect/") // Replace with the base URL of your API
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        API_Interface authService = retrofit.create(API_Interface.class);
+        String username = editTextLogInEmail.getText().toString();
+        String password = editTextLogInPassWord.getText().toString();
+        Call<HelperClass> call = authService.login("openremote",username,password,"password");
+        call.enqueue(new Callback<HelperClass>() {
+            @Override
+            public void onResponse(Call<HelperClass> call, Response<HelperClass> response) {
+                if (response.isSuccessful()) {
+                    HelperClass loginResponse = response.body();
+                    String accessToken = loginResponse.getAccess_token();
+                    Log.d("token",accessToken);
+                    Toast.makeText(SignUpActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    // Authentication failed
+                }
+            }
+            @Override
+            public void onFailure(Call<HelperClass> call, Throwable t) {
+                // Handle network or API errors
+                Log.d("API CALL", t.getMessage().toString());
+            }
+        });
+    }
+    @SuppressLint("SetJavaScriptEnabled")
+    private void loadWebView(String user,String email, String pass,String repass) {
+        String myURL = "https://uiot.ixxc.dev/auth/realms/master/protocol/openid-connect/auth?client_id=openremote&redirect_uri=https%3A%2F%2Fuiot.ixxc.dev%2Fmanager%2F&state=7b7ef2b3-64c3-4693-ba35-33e412d3c277&response_mode=fragment&response_type=code&scope=openid&nonce=c6011dc3-ac6e-46c3-9378-33fe07ab9bec";
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.removeAllCookies(null);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(myURL);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                Log.d("Url1",request.getUrl().toString());
+                if (request.getUrl().toString().contains("manager/#state")) {
+                    Log.d("Url","Go");
+                    login();
+                   // navigateToLoginMain();
+                    return true;
+                }
+                return false;
+            }
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+                String url = request.getUrl().toString();
+                //Log.d("Url",url);
+                return null;
+            }
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                webView.setVisibility(View.GONE);
+                if (url.contains("openid-connect/auth")) {
+                    String redirect = "document.getElementsByClassName('btn waves-effect waves-light')[1].click();";
+                    view.evaluateJavascript(redirect, null);
+                } else if (url.contains("login-actions/registration")){
+                    shouldStopEvaluation = false;
+                    String helperText = "document.getElementsByClassName('helper-text')[0].getAttribute('data-error');";
+                    String redText = "document.getElementsByClassName('red-text')[1].innerText;";
+                    view.evaluateJavascript(helperText, s -> {
+                        if (s.equals("null")) {
+                            view.evaluateJavascript(redText, s1 -> {
+                                if (s1.equals("null")){
+                                    String userField= "document.getElementById('username').value='" + user + "';";
+                                    String emailField= "document.getElementById('email').value='" + email + "';";
+                                    String passField= "document.getElementById('password').value='" + pass + "';";
+                                    String repassField= "document.getElementById('password-confirm').value='" + repass + "';";
+
+                                    view.evaluateJavascript(userField, null);
+                                    view.evaluateJavascript(emailField, null);
+                                    view.evaluateJavascript(passField, null);
+                                    view.evaluateJavascript(repassField, null);
+                                    view.evaluateJavascript("document.getElementsByTagName('form')[0].submit();", null);
+                                    Toast.makeText(SignUpActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    signUpErr(s1);
+                                }
+                            });
+                        } else {
+                            signUpErr(s);
+                        }
+                    });
+                }
+            }
+        });
+        webView.setWebChromeClient(new WebChromeClient());
+    }
+    private void signUpErr(String msg) {
+        buttonSignUpCreateAccount.setEnabled(true);
+        Toast.makeText(SignUpActivity.this, "Singup Failed" + msg, Toast.LENGTH_SHORT).show();
+    }
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -114,14 +229,20 @@ public class SignUpActivity extends AppCompatActivity {
                 Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
+
         });
 
         editTextSignUpUserName = findViewById(R.id.edt_signupUserName);
         editTextSignUpEmail = findViewById(R.id.edt_signupEmail);
         editTextSignUpPassWord = findViewById(R.id.edt_signupPassword);
         editTextSignUpConfirmPassword = findViewById(R.id.edt_sigupConfirmPassword);
+        editTextLogInEmail = findViewById(R.id.edt_loginEmail);
+        editTextLogInPassWord = findViewById(R.id.edt_loginPassword);
         buttonSignUpCreateAccount = findViewById(R.id.btn_signupCreateAccount);
 
+
+        webView = findViewById(R.id.webView);
+        //loadingProgressBar.setVisibility(View.VISIBLE);
 
         buttonSignUpCreateAccount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -129,21 +250,12 @@ public class SignUpActivity extends AppCompatActivity {
                 if (!validateUserName() | !validateEmail() | !validatePassword() | !validateConfirmPassword()) {
                     return;
                 }
-                database = FirebaseDatabase.getInstance();
-                reference = database.getReference("users");
-
-
-                String username = editTextSignUpUserName.getText().toString();
-                String email = editTextSignUpEmail.getText().toString();
-                String password = editTextSignUpPassWord.getText().toString();
-                String confirmpassword = editTextSignUpConfirmPassword.getText().toString();
-                //HelperClass helperClass = new HelperClass(username, email, password, confirmpassword);
-               // reference.child(username).setValue(helperClass);
-
-                Toast.makeText(SignUpActivity.this, "You have signup successfully!", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
-                startActivity(intent);
-
+                buttonSignUpCreateAccount .setEnabled(true);
+                final String user = String.valueOf(editTextSignUpUserName.getText());
+                final String email = String.valueOf(editTextSignUpEmail.getText());
+                final String pass = String.valueOf(editTextSignUpPassWord.getText());
+                final String repass = String.valueOf(editTextSignUpConfirmPassword.getText());
+                loadWebView(user,email,pass,repass);
             }
 
         });
